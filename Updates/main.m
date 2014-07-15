b6e6d3137d094635af8999910f46549ea25d67cd
Bryce Pauken <brycepauken@gmail.com>
2014-07-14 23:15:09 -0700

    Complete Revision

--- Updates/main.m
+++ Updates/main.m
@@ -1,18 +1,16 @@
 //
 //  main.m
-//  Update
+//  Updates
 //
-//  Created by Bryce Pauken on 5/15/14.
+//  Created by Bryce Pauken on 7/14/14.
 //  Copyright (c) 2014 Kingfish. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-#import "UPDAppDelegate.h"
-
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UPDAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
